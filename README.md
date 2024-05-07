591c4a41cc9e661e2d58f5c4f19cbbf1e0be5d2d
Daniel San Miguel <danielsanmiguel93@gmail.com>
2024-05-07 14:40:38 -0500

    updated everything

---
+++ README.md
@@ -0,0 +1,3 @@
+# Final_Project
+## Data Science Collaborative Project
+This is the final project for Applied Data Analysis 2024 Course taught by Dr. Anthony Difiore completed by Turner Lime, Nicholas Russell, Daniel San Miguel.
---
+++ doc/firingr8.R
@@ -0,0 +1,16 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(firingr8)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  library(firingr8)
+#  
+#  process_ephys_csv(csvfile_path = "raw_ephys_data.csv", experiment_identifier_path = "identifiers_sample.csv", output_path = ".")
+#  
+#  list.files()
+
